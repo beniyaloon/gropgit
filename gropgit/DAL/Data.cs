274946409ed1c33a32d7a5058d70a6d110ce351c
beniyaloon <y0548561556@gmail.com>
2024-08-05 09:54:26 +0300

    dal

---
+++ gropgit/DAL/Data.cs
@@ -0,0 +1,25 @@
+﻿namespace gropgit.DAL
+{
+    public class Data
+    {
+        private readonly string _connectionString = "server=BINYAMUN\\SA;initial catalog=shoes_and_hats; user id=sa;" +
+          "password=1234;TrustServerCertificate=Yes";
+        private static Data _data;
+        private DataLayer DataLayer;
+        private Data()
+        {
+            DataLayer = new DataLayer(_connectionString);
+        }
+        public static DataLayer Get
+        {
+            get
+            {
+                if (_data == null)
+
+                    _data = new Data();
+                return _data.DataLayer;
+
+            }
+        }
+    }
+}
---
+++ gropgit/DAL/DataLayer.cs
@@ -0,0 +1,29 @@
+﻿using gropgit.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace gropgit.DAL
+{
+    public class DataLayer : DbContext
+    {
+        public DataLayer(string ConnectionString) : base(GetOptions(ConnectionString))
+        {
+            Database.EnsureCreated();
+            //Seed("גמרא");
+        }
+        //private void Seed(string Genre)
+        //{
+        //    if (librariess.Any()) return;
+        //    Library library = new Library { Genre = Genre };
+        //    librariess.Add(library);
+        //    SaveChanges();
+        //}
+        private static DbContextOptions GetOptions(string ConnectionString)
+        {
+            return new DbContextOptionsBuilder().UseSqlServer(ConnectionString).Options;
+        }
+        public DbSet<Shoe> Shoes { get; set; }
+        //public DbSet<Shelf> shelfs { get; set; }
+        //public DbSet<Book> Books { get; set; }
+    }
+}
