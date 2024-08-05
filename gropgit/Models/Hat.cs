891c990ea83dce3d75a3b3c3566f68d4a526539f
Moshetzelniker <x0583200094@gmail.com>
2024-08-05 12:38:38 +0300

    Hat

---
+++ gropgit/Models/Hat.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gropgit.Models
+{
+    public class Hat
+    {
+
+        [Key]
+        public int HatId { get; set; }
+
+        [Display(Name = "מידה")]
+        public int Hatmeasure { get; set; }
+
+        [Display(Name = "צבע")]
+        public string HatColor { get; set; }
+
+        [Display(Name = "מותג")]
+        public string Hatbrand { get; set; }
+
+        [Display(Name = "תמונה")]
+        public string HatImage { get; set; }
+    }
+}
